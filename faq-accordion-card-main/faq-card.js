45c958bbd84ba558ab8d92793fc4de9272174595
Keamogetswe Mphakalasi <morobikmobi@gmail.com>
2021-03-02 22:53:15 +0200

    Implemented the animations of the faq card including on hover text highlighting, collapsible answers to questions, on click arrow rotations and closing already opened answers when others are clicked.

--- faq-accordion-card-main/faq-card.js
+++ faq-accordion-card-main/faq-card.js
@@ -1,8 +1,27 @@
-var questions = document.getElementByClassName('question');
+var questions = document.getElementsByClassName('question');
+var openedCollapsible = [];
 var i;
 
 for (i = 0; i < questions.length; i++) {
-	questions.addEventListener('click', function() {
-		
+	questions[i].addEventListener('click', function() {
+		var arrow = this.nextElementSibling;
+		var answer = this.nextElementSibling.nextElementSibling;
+		arrow.classList.toggle("arrow-rotate");
+		if (answer.style.maxHeight) {
+			answer.style.maxHeight = null;
+			openedCollapsible.pop();
+			this.style.fontWeight = "400";
+		} else {
+			if (openedCollapsible.length == 0) {
+				openedCollapsible.push(this);
+			} else {
+				var alreadyOpened = openedCollapsible.pop();
+				alreadyOpened.click();
+				openedCollapsible.push(this);
+			}
+			answer.style.maxHeight = answer.scrollHeight + "px";
+			this.style.fontWeight = "700";
+		}
+		this.classList.toggle("question-highlight");
 	});
 }
